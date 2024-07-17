44a8b8b90b8ef2f9c54ec1948be09bb04ef165af
monody <qq614908309@gmail.com>
2024-07-17 21:29:08 +0800

    Initial commit

---
+++ src/main/java/org/example/websocketbootdemo/WebsocketBootDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.websocketbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketBootDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/example/websocketbootdemo/config/MyWebSocketHandler.java
@@ -0,0 +1,2 @@
+package org.example.websocketbootdemo.config;public class MyWebSocketHandler {
+}
